0abcadfa86c74e5bd245d8d98bcffdb1754f9dba
RoboZe <roboticsxzero@gmail.com>
2025-08-08 16:27:21 +0530

    Hard problem solved

--- Phase1-Data-types and controlled-statements/Day 5/ATM_Simulation.py
+++ Phase1-Data-types and controlled-statements/Day 5/ATM_Simulation.py
@@ -22,6 +22,7 @@
 # Fisrt current amont stored as a int 
 
 current_amt = int(2500)
+user_pass = "12345"
 
 # We should make actions functions --> check amount , --> withdraw (w)(Subtract) , --> Deposit ( add)(D)
 
@@ -34,7 +35,7 @@ def check_amt(amt):
 
 # Calling current_amt function 
 
-check_amt(current_amt)
+# check_amt(current_amt)
 
 
 # Making withdraw function 
@@ -47,8 +48,8 @@ def withdraw(amt):
     return withdraw_amt
 
 # checking withdraw function 
-amt = 500
-print(f'your withdraw amount is {withdraw(amt)}')
+# amt = 500
+# print(f'your withdraw amount is {withdraw(amt)}')
 
 
 def deposit(amt):
@@ -57,8 +58,46 @@ def deposit(amt):
      print(f'This amout is deposited : {amt}')
      return current_amt
 
-amt2= 500 
-print(f'After deposit your amount is : {deposit(amt2)}')
+# amt2= 500 
+# print(f'After deposit your amount is : {deposit(amt2)}')
+
+
+
+                           ### Main Script ### 
+
+# We will loop this according to users input 
+
+
+while True :
+    print("Good Greeting welcome to our ATM ")
+    user_input = input(" Do want to proceed Choose (Y/N) : " )
+
+    while user_input.lower()=='y':
+        print("Welcome to your account")
+
+        use_pass = input("Please enter your secret number : ")
+    
+        if use_pass== user_pass:
+            action_input= int( input(""" Enter 1 --> check current balance  ,
+                                    Enter 2 --> To withdraw Amount ,
+                                    Enter 3 --> To deposit  amount"""))
+            if action_input == 1:
+                check_amt(current_amt)
+            elif action_input == 2:
+                amount = int(input("Enter the amount you want to withdraw : "))   
+                print(f"{withdraw(amount)} is withdrawn from your account")
+            elif action_input ==3 :
+                depo_amount = int(input("Enter the amount you wanna deposit to your account : "))    
+                print(f'Current amount is : {deposit(depo_amount)}')
+            else:
+             print("Exiting the screen  ")    
+
+        else:  
+            break   
+
+
+        
+
 
 
 
