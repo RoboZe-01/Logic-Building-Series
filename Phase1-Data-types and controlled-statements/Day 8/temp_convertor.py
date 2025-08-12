d9c391cbca28f77ffed83bdac95a9b02204dbdcd
RoboZe <roboticsxzero@gmail.com>
2025-08-12 22:23:42 +0530

    Problem done

--- Phase1-Data-types and controlled-statements/Day 8/temp_convertor.py
+++ Phase1-Data-types and controlled-statements/Day 8/temp_convertor.py
@@ -20,3 +20,27 @@ def fahrenite_to_celcius(temp):
 #  Creating the loop to ask the question to make temperature convertor
 
 
+
+while True : 
+    print("Which value you want to convert to which ? ")
+    ask_user = input("""
+            1 . Enter 1 to convert celcius to kelvin
+            2 . Enter 2 to convert kelvin to celcius 
+            3 . Enter 3 to conver celcius to fahranite 
+            4 . Enter 4 to convert fahranite to celcius 
+                    
+
+                    Choose :""")
+    if ask_user== "1" :
+        user_value = int(input("Enter the temp in celcius ( only give int value ): "))
+        print( f' {user_value} is celcius is {celcius_to_kelvin(user_value)} in kelvin ')
+    elif ask_user== "2" :
+        user_value = int(input("Enter the temp in kelvin ( only give int value ): "))
+        print( f' {user_value} is kelvin is {kelvin_to_celcius(user_value)} in celcius')    
+    elif ask_user== "3" :
+        user_value = int(input("Enter the temp in celcius ( only give int value ): "))
+        print( f' {user_value} is celcius is {celcius_to_fahrenite(user_value)} in farhnite')    
+    elif ask_user== "4" :
+        user_value = int(input("Enter the temp in farhanite ( only give int value ): "))
+        print( f' {user_value} is farhanite is {fahrenite_to_celcius(user_value)} in celcius')    
+
