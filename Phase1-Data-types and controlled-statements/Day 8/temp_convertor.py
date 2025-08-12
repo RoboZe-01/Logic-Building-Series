11a3b50e4c66fa7c0c276e64514a1e74adf4cd5f
RoboZe <roboticsxzero@gmail.com>
2025-08-12 22:08:35 +0530

    Problem solving started

--- Phase1-Data-types and controlled-statements/Day 8/temp_convertor.py
+++ Phase1-Data-types and controlled-statements/Day 8/temp_convertor.py
@@ -1,6 +1,22 @@
 # Problem statements : COnvert celcius to feranite or kelvin
 
 
-
+## Function to convert celcius to kelvin 
 def celcius_to_kelvin(temp):
-    
+    return temp + 273.15
+
+# Function to convert kelvin to celcius 
+def kelvin_to_celcius(temp):
+    return temp - 273.15
+
+# Function to convert celcius to fahranite 
+def celcius_to_fahrenite(temp):
+    return ( temp*1.8) + 32
+
+# Function to convert fahranite to celcius 
+def fahrenite_to_celcius(temp):
+    return (temp -32)*1.8
+
+#  Creating the loop to ask the question to make temperature convertor
+
+
