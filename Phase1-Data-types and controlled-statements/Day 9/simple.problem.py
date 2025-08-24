26968573d0fb9fb078e4649dd3259596f7f583ef
Prem Kamble <roboticsxzero@gmail.com>
2025-08-24 23:48:57 +0530

    done

--- Phase1-Data-types and controlled-statements/Day 9/simple.problem.py
+++ Phase1-Data-types and controlled-statements/Day 9/simple.problem.py
@@ -1,4 +1,7 @@
 # problem statement : Getting a lucky number from user 
-
-input_user= int(input("Enter your lucky number here : "));
    
+# Get lucky number from the user
+input_user = int(input("Enter your lucky number here: "))
+
+# Print the lucky number
+print("Your lucky number is:", input_user)
