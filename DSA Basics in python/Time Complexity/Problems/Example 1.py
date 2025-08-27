da2c12e4c13e41cd7bfa11bc885f161ad3dcdae2
RoboZe <roboticsxzero@gmail.com>
2025-08-27 13:06:41 +0530

    problem starteed

---
+++ DSA Basics in python/Time Complexity/Problems/Example 1.py
@@ -0,0 +1,8 @@
+n=int(input("enter the worst case input number : "))
+
+for i in range(1,n+1):          # 
+    for j in range (1, n+1):
+        print("Hi")
+
+
+        ## Find time complexity for it 
