897f594ed6c6002a63d47d1d7878dbfa31cb409a
RoboZe <roboticsxzero@gmail.com>
2025-08-30 12:13:03 +0530

    armstrong problem is solved

--- DSA Basics in python/Extraction of digits/Armstrong_num.py
+++ DSA Basics in python/Extraction of digits/Armstrong_num.py
@@ -8,3 +8,29 @@ it is like , if we add the digits in the number of digit having the power of the
 if it is equal to the original number then it is armstrong number
 
 """
+
+              ###  Programm start from here ### 
+
+
+n = int(input("Enter the number : "))              # Taking input from the user 
+num = n                                            # assigning this to different vatible as we dont want to change original one 
+
+
+# Making a function to find the armstrong or not 
+
+def find_armstrong(num):              
+ nod = len(str(num))            # finding the length of the number ( used str as int dont have len method)
+ total = 0                      # inital total put to zero 
+ while num>0 :                  # loop to find the total value of armstrong style 
+  ld = num %10                  # Finding last digit 
+  total = total+(ld**nod)       # adding digit to armstrong 
+  num = num //10
+ return total 
+
+
+# calling the function and using it to a new programme
+if find_armstrong(num) == n :
+ print(f"Your number {n} is an armstrong number")
+else:
+ print(f'Your number {n} is not armstrong number ') 
+ 
