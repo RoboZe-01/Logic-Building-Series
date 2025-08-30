c93ccf384abae59bb98131ee3db3c5903f02668d
RoboZe <roboticsxzero@gmail.com>
2025-08-30 11:44:23 +0530

    30 aug 25 started

---
+++ DSA Basics in python/Extraction of digits/Armstrong_num.py
@@ -0,0 +1,10 @@
+# find out number is armstrong number or not 
+
+"""
+Armstrong number is when 
+1234 = 1^4 + 2^4 +3^4 + 4^4  == 1234
+
+it is like , if we add the digits in the number of digit having the power of the lenght of the number 
+if it is equal to the original number then it is armstrong number
+
+"""
--- DSA Basics in python/Extraction of digits/Check_palindrome.py
+++ DSA Basics in python/Extraction of digits/Check_palindrome.py
@@ -2,7 +2,36 @@
 # Palindrome -->             actual word == word in reverse 
 
 """
-steps : 1. igit extraction 
+steps : 1. digit extraction 
+        2. Making a varible to store the reverse digits 
+        3. then checking the logic 
 
 
 """
+
+n = int(input("Enter the number : "))
+num = n 
+
+# Function to check palindrome 
+
+def check_palindrome(num, num_reverse):
+    if num == num_reverse:
+        return "It is a palindrome"  
+    else :
+        return "It is not a palindrome"
+
+
+# Function to reverse the num
+def reverse_num(num):
+    result = 0
+    while num >0 :
+        ld =num%10
+        result = result*10 +ld
+        num = num//10
+    return result       
+    
+
+
+
+
+print(f"Your number is {check_palindrome(num , reverse_num(num))}")
